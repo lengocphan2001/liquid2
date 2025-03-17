1179fcf727146b01d49572af2f10df5f437fa7b6
lengocphan2001] <lengocphan503@gmail.com>
2025-03-17 16:55:47 +0700

    Add files

---
+++ assets/ega-changelog.js
@@ -0,0 +1,9 @@
+# EGA Sportswear
+
+## Change Log
+Những thay đổi, điều chỉnh trong các phiên bản cập nhật của theme
+
+
+
+## 1.0.0 (24/04/2023)
+1. Release EGA Sportswear v1.0.0
---
+++ assets/ega.js
@@ -0,0 +1,10 @@
+var egajs={
+    "version": "1",
+    "packages": {
+        "haravan-egany-tools": "2.0.0"
+    },
+    "pages": {},
+    "blogs": {},
+    "articles": {},
+    "egaTheme": {}
+}
